46df0628d8df898051621c376614c449337b341a
Sebastian Birk <sebastian.birk@gmail.com>
2025-11-30 15:35:15 +0100

    enable copilot

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,43 +1,46 @@
 return {
-	{
-		"github/copilot.vim",
+    {
+        "github/copilot.vim",
         lazy = true,
-		---event = "InsertEnter",
-		---config = function()
-		---	vim.g.copilot_no_tab_map = true
-		---	vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-		---	vim.g.copilot_filetypes = {
-		---		["*"] = false,
-		---		["lua"] = true,
-		---		["rust"] = true,
-		---		["c"] = true,
-		---		["c#"] = true,
-		---		["cpp"] = true,
-		---		["css"] = true,
-		---		["go"] = true,
-		---		["python"] = true,
-		---		["md"] = true,
-		---		["markdown"] = true,
-		---		["tex"] = true,
-		---		["javascript"] = true,
-		---		["typescript"] = true,
-		---		["svelte"] = true,
-		---		["matlab"] = true,
-		---		["html"] = true,
-		---		["asm"] = true,
-		---		["s"] = true,
-		---		["sql"] = true,
-		---	}
-		---end,
-	},
+        ---event = "InsertEnter",
+        config = function()
+            vim.g.copilot_no_tab_map = true
+            vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+            vim.g.copilot_filetypes = {
+                ["*"] = false,
+                ["lua"] = true,
+                ["rust"] = true,
+                ["c"] = true,
+                ["c#"] = true,
+                ["cpp"] = true,
+                ["css"] = true,
+                ["go"] = true,
+                ["python"] = true,
+                ["md"] = true,
+                ["markdown"] = true,
+                ["tex"] = true,
+                ["javascript"] = true,
+                ["typescript"] = true,
+                ["svelte"] = true,
+                ["matlab"] = true,
+                ["html"] = true,
+                ["asm"] = true,
+                ["s"] = true,
+                ["sql"] = true,
+                ["exs"] = true,
+                ["ex"] = true,
+                ["elixir"] = true,
+            }
+        end,
+    },
     {
         "nvim-lua/plenary.nvim",
         lazy = true,
     },
     {
         "CopilotC-Nvim/CopilotChat.nvim",
-		config = function()
-			require("CopilotChat").setup({})
-		end,
+        config = function()
+            require("CopilotChat").setup({})
+        end,
     }
 }
